afbb3539fa7122bd774063c25e3889e4575c0acd
John Z Wu <bogardon@gmail.com>
2014-09-27 12:17:52 -0700

    generated campaign controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   root 'welcome#index'
   
   resources :collaborations
+  resources :campaigns
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
