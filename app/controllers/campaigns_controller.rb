7b08145099fca8e529308e9b2d6417599ba8a0d0
Anthony Liang <liangtfm@gmail.com>
2014-09-29 21:52:38 -0400

    Add index view and create form for collaborations

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -1,2 +1,22 @@
 class CampaignsController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def destroy
+  end
 end
--- app/controllers/collaborations_controller.rb
+++ app/controllers/collaborations_controller.rb
@@ -1,3 +1,39 @@
 class CollaborationsController < ApplicationController
-  
+  def new
+    @user = current_user
+    @collaboration = @user.collaborations.build
+  end
+
+  def create
+    @user = current_user
+
+    if @user.collaborations.create!(collab_params)
+      redirect_to collaborations_url
+    else
+      redirect_to collaborations_url
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def index
+    @collaborations = current_user.collaborations
+  end
+
+  def show
+  end
+
+  def destroy
+  end
+
+  private
+
+  def collab_params
+    params.require(:collaboration).permit(:name, :description)
+  end
+
 end
