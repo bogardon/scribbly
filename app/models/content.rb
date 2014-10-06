f03be9067474a9768662b03eb3820ed428badc69
John Z Wu <bogardon@gmail.com>
2014-10-05 17:25:54 -0700

    create contents

---
+++ app/models/content.rb
@@ -0,0 +1,9 @@
+class Content < ActiveRecord::Base
+  belongs_to :post
+  
+  # Paperclip
+  has_attached_file :media,
+                    :styles => { :medium => "300x300#", :thumb => "100x100#" },
+                    :default_url => "/images/:style/missing.png"
+  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+end
