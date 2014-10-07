71d2acca81ddf1503eb78fa1ce2d964879f015fe
John Z Wu <bogardon@gmail.com>
2014-10-06 22:07:06 -0700

    shit form

--- app/controllers/collaborations_controller.rb
+++ app/controllers/collaborations_controller.rb
@@ -1,3 +1,24 @@
 class CollaborationsController < ApplicationController
   
+def index
+  @collaborations = Collaboration.all
+end
+
+def new
+  @collaboration = Collaboration.new
+end
+
+def create
+  @collaboration = Collaboration.new(collaboration_params)
+  if @collaboration.save
+    redirect_to :collaborations
+  end
+end
+
+private
+
+def collaboration_params
+  params.require(:collaboration).permit(:name)
+end
+  
 end
